470d82bdfc5cafd0058421dbbf8f6598bdcde4ef
mario <mario@test.com>
2026-08-26 19:10:21 -0300

    dante honorato 2321406

--- REGISTROS_PARTICIPANTE.md
+++ REGISTROS_PARTICIPANTE.md
@@ -27,3 +27,30 @@ Caso utilize este espaço, organize o conteúdo da maneira que preferir.
 ---
 
 Registros
+
+###SEGUE ABAIXO MEUS REGISTROS ################
+
+------------- OBSERVACAO IMPORTANTE  ------------
+Nao tenho experiencia com javascript, todo meu codigo que fiz foi seguindo padroes e sintaxes parecidas com python, por isso nao consegui acabar todo o desafio
+
+Porem para finalizar ele, o que faltou foi apenas a as celulas ativarem depois de fazer o undo/redo depois do jogo terminar, o que eu faria rodando o loop de ativas as celulas caso o checkgame desse true ao apertar o undo/redo (antes deles atualizarem os boards) assim as celulas manteriam ativadas
+
+----REFATORACOES FEITAS E PLANEJADAS ----------------
+Criei um helper checkEndgame que verifica se o jogo terminou ou nao, pois eu repetiria o codigo nas funcoes do redo e undo.
+
+Tambem deixei alguns comentarios no codigo, entre eles em uma funcao que utiliza metodos depreciados que para melhores praticas deveria ser trocado por outro (nao troquei pois nao tive tempo)
+
+function setStatus(msg, cls = '') {
+  status.textContent = msg;
+  status.className   = 'status' + (cls ? ` ${cls}` : ''); // METODO status DEPRECEADO
+}
+
+eu tambem refatoraria o css juntando os elementos que tem mudancas iguais e refatoraria o html para nao usar css inline, pois é melhor pratica juntar todo o css
+
+eu tambem adicionaria mais comentarios ao codigo existente e caso tivesse mais tempo revisaria o codigo dos testes pois ate quando meu codigo tinha falhas visiveis todos os testes ainda passavam, o que contradiz seus propositos.
+
+
+
+--------REFERENCIAS---------
+
+Por mais que eu nao saiba muito javascript, ja mexi um pouco antes com eventhandlers de botoes dele, e boa parte da sintaxe desse trabalho eram similares a python e C. entao consegui fazer boa parte dele, apenas nao tive tempo de reativar as cedulas depois de um redo/undo depois de terminar o jogo
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 // WINNING_COMBOS, check, getNextPlayer, applyMove, createInitialState
 // are provided by game.js, loaded before this script.
 
@@ -7,8 +9,18 @@ const cells    = document.querySelectorAll('.cell');
 const status   = document.getElementById('status');
 const restartBtn     = document.getElementById('restart');
 
+const undoBtn     = document.getElementById('undo');
+const redoBtn     = document.getElementById('redo');
+
+let undoCounter = 0
+let redoCounter = 0
+
 let data = createInitialState();
-const board = document.getElementById('board');
+const board = document.getElementById('board'); // VARIAVEL NAO USADA
+
+let prevBoard;
+
+let redoBoard;
 
 function render() {
   cells.forEach((cell, i) => {
@@ -20,21 +32,12 @@ function render() {
 
 function setStatus(msg, cls = '') {
   status.textContent = msg;
-  status.className   = 'status' + (cls ? ` ${cls}` : '');
+  status.className   = 'status' + (cls ? ` ${cls}` : ''); // METODO DEPRECEADO
 }
 
-function handleClick(e) {
-  const idx = Number(e.currentTarget.dataset.index);
-  if (data.board[idx] || data.gameOver) return;
-
-  const nextBoard = applyMove(data.board, idx, data.current);
-  if (!nextBoard) return;
-  data.board = nextBoard;
-  render();
-
-  // Animate the placed cell
-  cells[idx].classList.add('placed');
-
+//HELPER FEITO
+function checkEndGame()
+{
   const result = check(data.board);
 
   if (result) {
@@ -47,11 +50,42 @@ function handleClick(e) {
     }
     // Disable all cells
     cells.forEach(c => (c.disabled = true));
-    return;
+    
+        
+    return true;
+  }
+  return false
+}
+
+
+function handleClick(e) {
+
+  undoCounter = 0;
+  redoCounter = 0;
+
+  const idx = Number(e.currentTarget.dataset.index);
+  if (data.board[idx] || data.gameOver) return;
+
+  prevBoard = data.board
+
+  const nextBoard = applyMove(data.board, idx, data.current);
+  if (!nextBoard) return;
+  data.board = nextBoard;
+  render();
+
+  redoBoard = data.board
+
+  // Animate the placed cell
+  cells[idx].classList.add('placed');
+
+
+  if (checkEndGame())
+  {
+    return
   }
 
-  data.current = getNextPlayer(data.current);
-  setStatus(`Player ${data.current}'s turn`);
+  data.current = getNextPlayer(data.current); 
+  setStatus(`Player ${data.current}'s turn `); 
 }
 
 function restartGame() {
@@ -60,8 +94,63 @@ function restartGame() {
   setStatus(`Player ${data.current}'s turn`);
 }
 
+function undo(){  
+
+  if (undoCounter >= 1 || !prevBoard)
+  {
+    return
+  }
+
+  if (checkEndGame())
+  {
+    cells.forEach(c => (c.disabled = false));
+  }
+
+  data.current = getNextPlayer(data.current);
+  setStatus(`Player ${data.current}'s turn `);
+  redoCounter = 0;
+  undoCounter++;
+  data.board = prevBoard
+  render()
+  
+
+}
+
+
+
+function redo(){  
+
+
+  if (redoCounter >= 1 || !prevBoard)
+  {
+    return
+  }
+
+
+  if (checkEndGame())
+  {
+    cells.forEach(c => (c.disabled = false));
+  }
+
+
+  data.current = getNextPlayer(data.current);
+  setStatus(`Player ${data.current}'s turn `);
+  undoCounter = 0;
+  redoCounter++;
+  data.board = redoBoard
+  render()
+  checkEndGame();
+
+
+}
+
+
 cells.forEach(cell => cell.addEventListener('click', handleClick));
 restartBtn.addEventListener('click', restartGame);
+
+
+undoBtn.addEventListener('click', undo);
+redoBtn.addEventListener('click', redo);
 
 // Initial render
 render();
